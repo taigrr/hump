bf42402d52ad245d0f18ce44cb5b4bea1a27b091
Tai Groot <tai@taigrr.com>
2025-04-04 15:19:18 -0700

    formatter

--- class.lua
+++ class.lua
@@ -22,19 +22,20 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-]]--
+]]
+--
 
 local function include_helper(to, from, seen)
 	if from == nil then
 		return to
-	elseif type(from) ~= 'table' then
+	elseif type(from) ~= "table" then
 		return from
 	elseif seen[from] then
 		return seen[from]
 	end
 
 	seen[from] = to
-	for k,v in pairs(from) do
+	for k, v in pairs(from) do
 		k = include_helper({}, k, seen) -- keys might also be tables
 		if to[k] == nil then
 			to[k] = include_helper({}, v, seen)
@@ -56,9 +57,11 @@ end
 
 local function new(class)
 	-- mixins
-	class = class or {}  -- class can be nil
+	class = class or {} -- class can be nil
 	local inc = class.__includes or {}
-	if getmetatable(inc) then inc = {inc} end
+	if getmetatable(inc) then
+		inc = { inc }
+	end
 
 	for _, other in ipairs(inc) do
 		if type(other) == "string" then
@@ -69,30 +72,34 @@ local function new(class)
 
 	-- class implementation
 	class.__index = class
-	class.init    = class.init    or class[1] or function() end
+	class.init = class.init or class[1] or function() end
 	class.include = class.include or include
-	class.clone   = class.clone   or clone
+	class.clone = class.clone or clone
 
 	-- constructor call
-	return setmetatable(class, {__call = function(c, ...)
-		local o = setmetatable({}, c)
-		o:init(...)
-		return o
-	end})
+	return setmetatable(class, {
+		__call = function(c, ...)
+			local o = setmetatable({}, c)
+			o:init(...)
+			return o
+		end,
+	})
 end
 
 -- interface for cross class-system compatibility (see https://github.com/bartbes/Class-Commons).
 if class_commons ~= false and not common then
 	common = {}
 	function common.class(name, prototype, parent)
-		return new{__includes = {prototype, parent}}
+		return new({ __includes = { prototype, parent } })
 	end
 	function common.instance(class, ...)
 		return class(...)
 	end
 end
 
-
 -- the module
-return setmetatable({new = new, include = include, clone = clone},
-	{__call = function(_,...) return new(...) end})
+return setmetatable({ new = new, include = include, clone = clone }, {
+	__call = function(_, ...)
+		return new(...)
+	end,
+})
--- signal.lua
+++ signal.lua
@@ -22,7 +22,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-]]--
+]]
+--
 
 local Registry = {}
 Registry.__index = function(self, key)
@@ -45,41 +46,49 @@ function Registry:emit(s, ...)
 end
 
 function Registry:remove(s, ...)
-	local f = {...}
-	for i = 1,select('#', ...) do
+	local f = { ... }
+	for i = 1, select("#", ...) do
 		self[s][f[i]] = nil
 	end
 end
 
 function Registry:clear(...)
-	local s = {...}
-	for i = 1,select('#', ...) do
+	local s = { ... }
+	for i = 1, select("#", ...) do
 		self[s[i]] = {}
 	end
 end
 
 function Registry:emitPattern(p, ...)
 	for s in pairs(self) do
-		if s:match(p) then self:emit(s, ...) end
+		if s:match(p) then
+			self:emit(s, ...)
+		end
 	end
 end
 
 function Registry:registerPattern(p, f)
 	for s in pairs(self) do
-		if s:match(p) then self:register(s, f) end
+		if s:match(p) then
+			self:register(s, f)
+		end
 	end
 	return f
 end
 
 function Registry:removePattern(p, ...)
 	for s in pairs(self) do
-		if s:match(p) then self:remove(s, ...) end
+		if s:match(p) then
+			self:remove(s, ...)
+		end
 	end
 end
 
 function Registry:clearPattern(p)
 	for s in pairs(self) do
-		if s:match(p) then self[s] = {} end
+		if s:match(p) then
+			self[s] = {}
+		end
 	end
 end
 
@@ -95,8 +104,10 @@ local default = Registry.new()
 local module = {}
 for k in pairs(Registry) do
 	if k ~= "__index" then
-		module[k] = function(...) return default[k](default, ...) end
+		module[k] = function(...)
+			return default[k](default, ...)
+		end
 	end
 end
 
-return setmetatable(module, {__call = Registry.new})
+return setmetatable(module, { __call = Registry.new })
